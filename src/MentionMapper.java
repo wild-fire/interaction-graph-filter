ab99b584e76e569dc9f682199fcd6667ed97454f
David Brenes <davidjbrenes@gmail.com>
2015-03-16 00:54:47 +0100

    Changing the whole mapper class to use other JSON Parser and parse the dates

--- src/MentionMapper.java
+++ src/MentionMapper.java
@@ -1,51 +1,55 @@
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.Locale;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
-import com.codesnippets4all.json.parsers.JSONParser;
-import com.codesnippets4all.json.parsers.JsonParserFactory;
-
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MentionMapper extends Mapper<Object, Text, Text, Interaction> {
-	
 
-	 JSONParser parser = JsonParserFactory.getInstance().newJsonParser();
-	 DateFormat dateFormat = DateFormat.getDateInstance();
-	 
+	JsonParser parser = new JsonParser();
+	final String TWITTER_DATE_FORMAT = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+	SimpleDateFormat dateInputFormat = new SimpleDateFormat(TWITTER_DATE_FORMAT, Locale.ENGLISH);
+	SimpleDateFormat dateOutputFormat = new SimpleDateFormat("yyyy-MM", Locale.ENGLISH);
+
 	public void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
-		// We get the file line which is "tweet_id \t tweet_json" and then retrieve the json tweet
+		// We get the file line which is "tweet_id \t tweet_json" and then
+		// retrieve the json tweet
 		String[] tweetLine = value.toString().split("\t", 2);
 		@SuppressWarnings("unchecked")
-		Map<String, Object> tweet = parser.parseJson(tweetLine[1]);
-		
-		Map<String, Object> user = (Map<String, Object>) tweet.get("user");
-		String userId = user.get("id_str").toString();
-		
+		JsonObject tweet = parser.parse(tweetLine[1]).getAsJsonObject();
+
+		String userId = tweet.get("user").getAsJsonObject().get("id_str")
+				.getAsString();
+
 		Date tweetDate = new Date();
 		try {
-			tweetDate = dateFormat.parse(tweet.get("created_at").toString());
+			tweetDate = dateInputFormat.parse(tweet.get("created_at").getAsString());
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+
+		String outputDate = dateOutputFormat.format(tweetDate);
 		
 		@SuppressWarnings("unchecked")
-		Map<String, Map<String, Object>[]> entities = (Map<String, Map<String, Object>[]>) tweet.get("entities");
-		
-		for(Map<String, Object> mentionedUser: entities.get("user_mentions")){
-			Interaction mention = new Interaction(userId, mentionedUser.get("id_str").toString() );
-			context.write(new Text(tweetDate.toString()), mention);
-		};
-		
+		JsonObject entities = tweet.get("entities").getAsJsonObject();
+
+		for (JsonElement mentionedUser : entities.get("user_mentions")
+				.getAsJsonArray()) {
+			Interaction mention = new Interaction(userId, mentionedUser
+					.getAsJsonObject().get("id_str").toString());
+			context.write(new Text(outputDate), mention);
+		}
+		;
 
 	}
 
