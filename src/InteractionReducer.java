a1666d65ce8596c7601d22efa0b139f1ffa18713
David Brenes <davidjbrenes@gmail.com>
2015-03-16 01:34:06 +0100

    Enabling multiple outputs so results are splitted in a file per month

--- src/InteractionReducer.java
+++ src/InteractionReducer.java
@@ -2,19 +2,26 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 
 public class InteractionReducer extends
 		Reducer<Text, Interaction, Text, Text> {
 
-	private IntWritable result = new IntWritable();
+	private MultipleOutputs<Text, Text> multipleOutputs;
+	
+	public void setup(Context context) {
+	  this.multipleOutputs = new MultipleOutputs<Text, Text>(context);
+	}
+	
+	public void cleanup(Context c) throws IOException, InterruptedException {
+		this.multipleOutputs.close();
+	}
 	
 	public void reduce(Text key, Iterable<Interaction> values,
 			Context context) throws IOException, InterruptedException {
-		int length = 0;
 		Map<String, Map<String, Integer>> interactions = new HashMap<String, Map<String, Integer>>();
 		
 		for (Interaction i : values) {
@@ -29,13 +36,12 @@ public class InteractionReducer extends
 				userInteractions.put(i.getInteractedUser(), totalInteractions + 1);
 			}
 			interactions.put(i.getInteractingUser(), userInteractions);
-			length += 1;
 		}
 		
 		for (String interactingUser : interactions.keySet()) {
 			Map<String, Integer> userInteractions = interactions.get(interactingUser);			
 			for(String interactedUser : userInteractions.keySet()) {
-				context.write(key, new Text(interactingUser + "\t" + interactedUser + "\t" + userInteractions.get(interactedUser)));
+				this.multipleOutputs.write( new Text(interactingUser), new Text(interactedUser + "\t" + userInteractions.get(interactedUser)), key.toString());
 			}
 		}
 		
